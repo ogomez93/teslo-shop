7551c7a1e54caaa25e024d71aecbc2c92e115447
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-05-29 22:48:33 -0300

    Adds ShopLayout to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import { Typography } from '@mui/material'
+
+import { ShopLayout } from '../components/layouts'
 
 const Home: NextPage = () => {
   return (
-    <h1>
-      Teslo Shop
-    </h1>
+    <ShopLayout title='Teslo-Shop - Home' pageDescription='Find the best Teslo products here'>
+      <Typography variant='h1' component='h1'>Store</Typography>
+      <Typography variant='h2' component='h2' sx={{ mb: 1 }}>All Products</Typography>
+    </ShopLayout>
   )
 }
 
