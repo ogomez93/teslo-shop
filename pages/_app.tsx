b6d605b5a2c306aebc8036c15a8ec78381d8159f
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-05-29 22:36:57 -0300

    Adds theme, products seed, and assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@emotion/react'
+
+import '../styles/globals.css'
+import { lightTheme } from '../themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
